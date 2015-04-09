ab1ffc48d00dd2875a865b3c23e6141b734739d2
Irving Popovetsky <irving@getchef.com>
2015-04-09 11:55:54 -0700

    Fix bug with language tag parsing

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -11,6 +11,7 @@ module HighlightCode
     lang = 'objc' if lang == 'm'
     lang = 'perl' if lang == 'pl'
     lang = 'yaml' if lang == 'yml'
+    lang = lang.gsub(/<.*>/, '') # strip out any tags which mightve made it into the lang value
     str = pygments(str, lang).match(/<pre>(.+)<\/pre>/m)[1].to_s.gsub(/ *$/, '') #strip out divs <div class="highlight">
     tableize_code(str, lang)
   end
